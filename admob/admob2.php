e8f708676df8c9ce1c6c9ddcc09b42d604942491
Anthony James <anthony@adjames.com>
2011-11-09 00:09:49 -0600

    addmob first commit

---
+++ admob/admob2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html> 
+<html> 
+ <head> 
+	<title>Page Title</title> 
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0b1/jquery.mobile-1.0b1.min.css" />
+<script src="http://code.jquery.com/jquery-1.6.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.0b1/jquery.mobile-1.0b1.min.js"></script>
+ 	<meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1">
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
+
+
+<style type="text/css">
+		.custom_header { position:fixed;}
+	</style>
+ </head> 
+ 
+
+<body> 
+ <div data-role="page" data-theme="a" id="index" data-backbtn="false"> 
+ 	<div data-role="header" data-theme="a" class="custom-header">
+			<h1>Pinehead.tv</h1>
+	</div>
+	<div data-role="content" data-theme="b"> 
+	
+		<?php include('admob_js.php'); ?>
+
+	</div> 
+	
+</div> 
+
+
+
+
+</body>
+</html>
---
+++ admob/admob_js.php
@@ -0,0 +1,29 @@
+<div id="admob_ad" class="admob"></div>
+<script type="text/javascript">
+      var admob_vars = {pubid: 'a14da9193f5e7dc',
+                        bgcolor: 'FF9119',
+                        text: 'FFFFFF',
+                        ama: false,
+                        test: false,
+                        manual_mode: true };
+</script>
+<script type="text/javascript" src="http://mmv.admob.com/static/iphone/iadmob.js"></script>
+<script type="text/javascript">
+      var fnFoo = function(){
+            var ad = $('#admob_ad',this)[0];
+            if (typeof ad != 'undefined') {
+                  ad.innerHTML = '';
+                 	//request an add we can do this because <script> is called iadmob.js
+                  _admob.fetchAd(ad);
+                  
+            }
+            $('div[data-role=page]').die('pageshow', fnFoo);
+       };
+       $('div[data-role=page]').live('pageshow', fnFoo);
+	
+</script>	
+
+
+	
+	
+	
